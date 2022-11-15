a472b6acc773a072bbee48d435b54e2237f85e92
TamarawGuy <viktor.m.rumenov@gmail.com>
2022-11-15 19:41:32 +0200

    Implemented navigation bar component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
 
-import './Header.css';
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        {" "}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae
+        recusandae accusamus earum quisquam labore error, dolores voluptatem
+        veniam dicta animi.
+      </p>
+      <button className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header" />
+    </div>
   </div>
 );
 
